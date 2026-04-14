63da8bfe53389b1b6da85923b289679eca8ce8dc
Marcos Gallardo <marcos.gallardo@estech.local>
2026-04-14 09:11:22 +0200

    prueba real time

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { supabase } from '@/lib/supabase'
 import Inventario from './components/Inventario'
 
+export const revalidate = 0
+
 export default async function Home() {
   const { data: inventario } = await supabase
     .from('Inventario')
